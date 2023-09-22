341e4f376270f94bd05855ab73302959028728d2
srinadh7 <121044889+srinadhkesineni@users.noreply.github.com>
2023-09-22 06:43:09 +0530

    Time: 494 ms (27.90%), Space: 33.4 MB (19.67%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,12 +3,9 @@ class Solution:
       d={}
       for i in nums:
         if i in d:
-          d[i]+=1
+          return True
         if i not in d:
           d[i]=1
-      for key , value in d.items():
-        if value>1:
-          return True
       return False
 
 
