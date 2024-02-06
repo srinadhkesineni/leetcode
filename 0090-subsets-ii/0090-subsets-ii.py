8d1728e05729a7f74f4d9f67de6e7e1fcefaf584
srinadh7 <121044889+srinadhkesineni@users.noreply.github.com>
2024-02-06 16:52:44 +0530

    Time: 17 ms (75.10%), Space: 11.7 MB (96.15%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    
+
+
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        total=[]
+        ds=[]
+        def func(idx,arr,ds):
+            if idx==len(arr):
+                if ds not in total:
+                    total.append(ds[:])
+                return
+            ds.append(arr[idx])
+            func(idx+1,arr,ds)
+            ds.pop(-1)
+            while idx+1 <len(arr) and nums[idx]==nums[idx+1]:
+                idx+=1
+            func(idx+1,arr,ds)
+            return 
+        nums.sort()
+        func(0,nums,ds)
+        return total
+            
+
+        
